90ea95a48c71e14712b7cec3e3acca9e2928beb2
donbeo <lucapuggio@gmail.com>
2013-12-27 22:23:24 +0100

    add comment and remove useless dataset

--- stability_selection.py
+++ stability_selection.py
@@ -6,27 +6,20 @@ Created on Wed Dec 25 21:00:32 2013
 """
 
 
-#data 1
-diabetes = sk.datasets.load_diabetes()
-X = diabetes.data
-y = diabetes.target
-
-#data 2
-
 def stability_selection(X,y,alphas=False,sim=10,min_weight=0.2,plot_me=False):
     # this is a very beta version
     import numpy as np
     import sklearn as sk
     import sklearn.linear_model    
-    (n,p)=X.shape
-    X=sk.preprocessing.scale(X)
+    (n,p)=X.shape #n is the number of samples and p the number of variables
+    X=sk.preprocessing.scale(X) 
     y=sk.preprocessing.scale(y)
     #set the lasso path   
     if alphas.any():    
         alphas, coefs_lasso, _ = sk.linear_model.lasso_path(X, y,return_models=False,fit_intercept=False)
     
     prob=np.zeros(shape=(p,alphas.shape[0]))
-    
+    # run random lasso 
     for i in range(sim):
         w=np.random.uniform(low=min_weight,high=1,size=p)
         samples=np.random.randint(low=0,high=n-1,size=int(n/2))    
@@ -47,19 +40,20 @@ def stability_selection(X,y,alphas=False,sim=10,min_weight=0.2,plot_me=False):
     return max_prob,prob,alphas
 
 
+import numpy as np
+# create a sample dataset
 p=200
 n=200
-
 sigma=np.diag([1.]*p)
 sigma[0,2]=0.6
 sigma[2,0]=0.6
 sigma[1,2]=0.6
 sigma[2,1]=0.6
-
 X=np.random.multivariate_normal(mean=np.zeros(shape=(p,)),cov=sigma,size=n)
 beta=np.zeros(shape=(p,1))
 beta[0]=beta[1]=1
 y=np.dot(X,beta).squeeze()
-    
+# choose the lasso path    
 alphas=np.arange(0.0001,1,step=0.0051)
+#run stability selection on our samples dataset
 l=stability_selection(X,y,alphas=alphas,sim=100,min_weight=0.2,plot_me=True)
